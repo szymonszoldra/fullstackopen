5b873417f039353c568b7a6830540ae73376522c
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-19 21:28:49 +0100

    1.2: course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,18 +6,16 @@ const Header = ({ course }) => {
   )
 }
 
+const Part = ({ part, exercise }) => {
+  return <p>{part} {exercise}</p>
+}
+
 const Content = ({ exercises1, exercises2, exercises3, part1, part2, part3 }) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
     </div>
   )
 }
