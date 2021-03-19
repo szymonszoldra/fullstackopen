d62f0715cc1d348e5811018b30f1c61aa8f1a179
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-20 00:20:33 +0100

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,7 +10,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all;
+  
   return (
     <div>
       <h2>give feedback</h2>
@@ -22,6 +25,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average ? average : 0}</p>
+      <p>positive {positive ? positive : 0} %</p>
     </div>
   )
 }
