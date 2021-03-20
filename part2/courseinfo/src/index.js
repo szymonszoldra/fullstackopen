e6192b23c12c8f23dc767e37f4b76479c522bf6c
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-20 18:49:51 +0100

    2.2: Course information step7

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -7,12 +7,12 @@ const Header = ({ course }) => {
   )
 }
 
-// const Total = ({ course }) => {
-//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-//   return(
-//     <p>Number of exercises {sum}</p>
-//   ) 
-// }
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
 
 const Part = (props) => {
   return (
@@ -37,6 +37,7 @@ const Course = ({ course }) => {
     <>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
