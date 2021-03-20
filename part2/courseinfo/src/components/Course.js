c63d9bd5279479c85fba63a17a26939b888582d5
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-20 19:08:12 +0100

    2.5: separate module

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((acc, part) => acc+= part.exercises, 0);
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((singlePart) => (
+        <Part key={singlePart.id} part={singlePart} />
+      ))}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  )
+}
+
+export default Course;
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,46 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((acc, part) => acc+= part.exercises, 0);
-  return(
-    <p>Number of exercises {sum}</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((singlePart) => (
-        <Part key={singlePart.id} part={singlePart} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
