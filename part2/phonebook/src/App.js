9045757d51348e7fa1d4798282cbee7589e351b4
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-23 22:21:26 +0100

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({handler, value}) => <div>filter shown with <input onChange={handler} value={value}/></div>
 
@@ -27,15 +28,16 @@ const Form = ({formHandler, nameHandler, nameValue, numberHandler, numberValue})
 )
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]); 
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filterName, setFilterName ] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data));
+  },[]);
+
 
   const userAlreadyExists = () => {
     const user = persons.filter((person) => person.name.toLowerCase() === newName.toLowerCase());
