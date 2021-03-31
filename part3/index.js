a8f85f7ec7c6b2f187533ec95f8a454e239f3cdf
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-31 17:32:27 +0200

    3.7: Phonebook backend step7

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
 const { response } = require('express');
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
   {
