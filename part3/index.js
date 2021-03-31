2fc85a9936a5c8e4ddacc149c6f64bd3081ee2ba
szymonszoldra <szymon.szoldra@gmail.com>
2021-03-31 17:46:24 +0200

    3.8*: Phonebook backend step8

--- part3/index.js
+++ part3/index.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '));
 
 let persons = [
   {
