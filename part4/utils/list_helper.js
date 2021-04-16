5c81a71cdcb4c8b88656e1a0396dc3f79f7e2332
szymonszoldra <szymon.szoldra@gmail.com>
2021-04-16 11:32:10 +0200

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,7 +4,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (acc, post) => acc + post.likes;
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
 
