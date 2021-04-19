13b6bb57533818b2c2d8a60861abf8a55424ac64
szymonszoldra <szymon.szoldra@gmail.com>
2021-04-19 08:39:40 +0200

    4.8: Blog list tests, step1

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,16 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
+  const savedBlog = await blog.save();
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const Blog = require('../models/blog');
+
+const helper = require('./test_helper');
+
+const app = require('../app');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog));
+
+  const promiseArray = blogObjects.map(blog => blog.save());
+  await Promise.all(promiseArray);
+});
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
+
+    const contents = response.body.map(blog => blog.title);
+    expect(contents).toContain('React patterns');
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
