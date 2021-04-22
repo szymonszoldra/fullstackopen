846bee0d780ce0bde5b741fc31446a33a4200496
szymonszoldra <szymon.szoldra@gmail.com>
2021-04-22 16:05:04 +0200

    4.20*: bloglist expansion, step8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,30 +3,24 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization?.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 } );
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
   if (!decodedToken?.id) {
     return response.status(401).json({ error: 'token missing or invalid' });
   }
-  console.log(decodedToken);
+
   const user = await User.findById(decodedToken.id);
-  console.log(user);
+
   const data = {
     title: request.body.title,
     author: request.body.author,
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -12,6 +12,14 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization?.toLowerCase().startsWith('bearer ')) {
+    request.token =  authorization.substring(7);
+  }
+  next();
+};
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
@@ -29,5 +37,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 };
