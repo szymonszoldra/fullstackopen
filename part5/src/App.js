7137e89ed58b5be0b3993bcc90a816398f2020eb
szymonszoldra <szymon.szoldra@gmail.com>
2021-04-29 17:34:50 +0200

    5.3: bloglist frontend, step3

--- part5/src/App.js
+++ part5/src/App.js
@@ -7,6 +7,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -41,6 +44,7 @@ const App = () => {
         'loggedBloglistUser', JSON.stringify(user)
       );
 
+      blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
@@ -49,8 +53,26 @@ const App = () => {
     }
   }
 
+  const handleAddNewBlog = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newBlog = await blogService.create({
+        title, author, url
+      });
+
+      setBlogs(blogs.concat(newBlog));
+      setTitle('');
+      setAuthor('');
+      setUrl('');
+    } catch (exception) {
+      console.error('Error : ', exception);
+    }
+  }
+
   const logout = () => {
     window.localStorage.removeItem('loggedBloglistUser');
+    blogService.setToken(null);
     setUser(null);
   }
 
@@ -85,6 +107,37 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={logout}>log out</button></p>
+      <form onSubmit={handleAddNewBlog}>
+      <h2>create new</h2>
+      <div>
+        title:
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
 const baseUrl = '/api/blogs';
 
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = async () => {
   const request = await axios.get(baseUrl);
   return request.data;
 }
 
-export default { getAll };
+const create = async (newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newBlog, config)
+  return response.data
+}
+
+export default { getAll, create, setToken };
