e0568026d9f01066a073cb5275e82b9a11a18ba9
szymonszoldra <szymon.szoldra@gmail.com>
2021-05-06 15:12:00 +0200

    5.18: bloglist end to end testing, step2

--- part5/cypress/integration/blogapp.spec.js
+++ part5/cypress/integration/blogapp.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      name: 'Szymon Sz',
+      username: 'szymon',
+      password: 'passwd'
+    };
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -9,5 +15,26 @@ describe('Blog app', function() {
     cy.contains('username');
     cy.contains('password');
     cy.contains('login');
+  });
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input[name="Username"]').type('szymon');
+      cy.get('input[name="Password"]').type('passwd');
+      cy.contains('login').click();
+
+      cy.contains('Szymon Sz logged in');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('input[name="Username"]').type('szymon');
+      cy.get('input[name="Password"]').type('wrongpassword');
+      cy.contains('login').click();
+
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid');
+    });
   });
 });
