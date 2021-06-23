c8563f0203add15a5ea84f5b457142a7d17240d1
szymonszoldra <szymon.szoldra@gmail.com>
2021-06-23 17:17:57 +0200

    6.14 Anecdotes and the backend, step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addAnecdote } from '../reducers/anecdoteReducer';
 import { displayNotification, hideNotification } from '../reducers/notificationReducer';
-
+import anecdotesService from '../services/anecdotes';
+ 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
-
-  const handleSubmit = (event) => {
+ 
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
-    dispatch(addAnecdote(
-      event.target.anecdote.value
-    ));
-    dispatch(displayNotification(`ADDED: ${event.target.anecdote.value}`));
-    setTimeout(() => dispatch(hideNotification()), 5000);
-
+    
+    const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
+    const newAnecdote = await anecdotesService.createNew(content);
+    dispatch(addAnecdote(newAnecdote));
+ 
+    dispatch(displayNotification(`ADDED: ${content}`));
+    setTimeout(() => dispatch(hideNotification()), 5000);
   };
-
+ 
   return ( 
     <section>
        <h2>create new</h2>
