7f75bf02f5a4ee6916e501a25b00f13a2e260667
szymonszoldra <szymon.szoldra@gmail.com>
2021-06-25 19:26:54 +0200

    7.18: comments, step2

--- part7/bloglist-extended/client/src/components/blog/Blog.component.js
+++ part7/bloglist-extended/client/src/components/blog/Blog.component.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBlog } from '../../redux/reducers/blogReducer';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Blog = ({ blog, loggedUser }) => {
   const [likes, setLikes] = useState(blog.likes);
+  const [comment, setComment] = useState('');
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -36,6 +38,18 @@ const Blog = ({ blog, loggedUser }) => {
     }
   };
 
+  const addComment = async (e) => {
+    e.preventDefault();
+    if (!comment) {
+      return;
+    }
+
+    await axios.post(`/api/blogs/${blog.id}/comments`, { content: comment });
+    blog.comments = blog.comments.concat(comment);
+    console.log();
+    setComment('');
+  };
+
   const hasUserCreatedThatBlog = blog.user.username === loggedUser;
 
   return (
@@ -61,6 +75,9 @@ const Blog = ({ blog, loggedUser }) => {
 
       </div>
       <h3>comments</h3>
+      <form onSubmit={addComment}>
+        <input type="text" value={comment} name="commentinput" onChange={(e) => setComment(e.target.value)} /><button>add comment</button>
+      </form>
       <ul>
         {blog.comments.map(comment => <li key={comment + blog.id}>{comment}</li>)}
         {blog.comments.length === 0 ? 'No comments for this blog!' : null}
