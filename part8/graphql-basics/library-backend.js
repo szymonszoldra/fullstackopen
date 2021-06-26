29b75fe7ba71b27ae5451467aa4992be490128bc
szymonszoldra <szymon.szoldra@gmail.com>
2021-06-26 20:18:14 +0200

    8.4: Books of an author

--- part8/graphql-basics/library-backend.js
+++ part8/graphql-basics/library-backend.js
@@ -92,7 +92,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -116,7 +116,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(book => book.author === args.author),
     allAuthors: () => authors
   },
 
