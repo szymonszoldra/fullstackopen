fa329a8e4c8890c3330c1d20907faceb3ab77967
szymonszoldra <szymon.szoldra@gmail.com>
2021-06-26 19:13:36 +0200

    8.2: All books

--- part8/graphql-basics/library-backend.js
+++ part8/graphql-basics/library-backend.js
@@ -92,13 +92,23 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
   }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 };
 
